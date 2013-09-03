207581532f8c76ce787a83d158f5b9b235649b09
Tim Reidel <reideltj@yahoo.com>
2013-09-02 20:17:43 -0700

    fixing build

--- Toolchain-BeagleBone-Black.cmake
+++ Toolchain-BeagleBone-Black.cmake
@@ -1,22 +1,17 @@
 # this one is important
 SET(CMAKE_SYSTEM_NAME Linux)
-# this one not so much
-SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
 SET(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc")
 SET(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++")
 
-# ensure we ahve a target environment
+# ensure we have a target environment
 IF(NOT(DEFINED ENV{STAGING}))
     MESSAGE(FATAL_ERROR "STAGING not defined")
 ENDIF()
 
 # setup the root path 
 SET(CMAKE_FIND_ROOT_PATH "$ENV{STAGING}")
-
-# add an extra directory to the header search path
-INCLUDE_DIRECTORIES("${CMAKE_FIND_ROOT_PATH}/usr/include")
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -30,7 +25,7 @@ SET(CMAKE_C_FLAGS "-march=armv7-a -mfloat-abi=hard -mfpu=neon")
 SET(CMAKE_CXX_FLAGS "-march=armv7-a -mfloat-abi=hard -mfpu=neon")
 
 # add extra directories to the system library search path
-LIST(APPEND CMAKE_SYSTEM_LIBRARY_PATH "${CMAKE_FIND_ROOT_PATH}/usr/lib/arm-linux-gnueabihf")
+LIST(APPEND CMAKE_SYSTEM_LIBRARY_PATH "/usr/lib/arm-linux-gnueabihf")
 
 # add the extra directories to the linker search path
 SET(LINKER_FLAGS "-Wl,-dynamic-linker,/lib/ld-linux-armhf.so.3 -Wl,-rpath-link,${CMAKE_FIND_ROOT_PATH}/usr/lib:${CMAKE_FIND_ROOT_PATH}/lib:${CMAKE_FIND_ROOT_PATH}/lib/arm-linux-gnueabihf")
