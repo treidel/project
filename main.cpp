ccb4eed0b1e896df639cd486d3cfbf851f74372b
Tim Reidel <reideltj@yahoo.com>
2013-09-24 20:12:28 -0700

    moving logging utils to common.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include "bluetooth/spp_server.h"
 #include "audio/audio_capturemgr.h"
 
-//#include <unistd.h>
 #include <stdlib.h>
 #include <getopt.h>
 #include <ev.h>
@@ -165,66 +164,6 @@ int main(int argc, char *argv[])
     // we should never get here
     LOG4CXX_FATAL(g_logger, "main loop exited");
     return -1;
-}
-
-const std::string to_string(const void *value_p)
-{
-  char buffer[22 + 1];
-  snprintf(buffer, 22, "0x%p", value_p);
-  return buffer;
-}
-
-const std::string to_string(size_t value)
-{
-    std::stringstream ss;
-    std::string s;
-    ss << value;
-    s = ss.str();
-    return s;
-}
-
-const std::string to_string(int value)
-{
-    std::stringstream ss;
-    std::string s;
-    ss << value;
-    s = ss.str();
-    return s;
-}
-
-const std::string to_string(uint8_t value)
-{
-    std::stringstream ss;
-    std::string s;
-    ss << (int)value;
-    s = ss.str();
-    return s;
-}
-
-const std::string to_string(const uint8_t *data_p, size_t length)
-{
-  char buffer[(2 * length) + 1];
-  for (int counter = 0; counter < length; counter++)
-  {
-      uint8_t value = *(data_p + counter);
-      snprintf(buffer + (2 * counter), 3, "%02X", value);
-  }
-  return buffer;
-}
-
-const std::string to_string(const char *string_p)
-{
-    std::string value(string_p);
-    return value;
-}
-
-const std::string to_string(float value)
-{
-    std::stringstream ss;
-    std::string s;
-    ss << value;
-    s = ss.str();
-    return s;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
